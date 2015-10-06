00e87ec5da96d4e3f67c672a15952e6c36aca86c
DrSpyke <corey.hart.neuro@gmail.com>
2015-10-05 23:35:14 -0400

    Update SpykeArchitecture.py

--- SpykeArchitecture.py
+++ SpykeArchitecture.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 28 01:45:58 2015
-
 @author: Corey Hart
-
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License or any later version.
+the Free Software Foundation; either version 3 of the License, or
+any later version.
 """
 
 import numpy as np
@@ -15,8 +14,11 @@ class connection(object):
     #weights = np.array()
     def __init__(self,size,scale):
         self.weights = scale*np.random.rand(size,size)
-
-
+        
+    def update(self,history,currenttime,timestep,learning_rule = 'STDP', params = [0.1,1.0]):
+        if learning_rule == 'STDP':
+            self.weights[history[currenttime],history[currenttime - timestep]] += np.exp(-1.0*(currenttime - timestep))
+            
             
 class neuron(object):
     
@@ -29,16 +31,16 @@ class neuron(object):
         self.t_rest  = 0                   # initial refractory time
         self.last_t  = 0                   # last time
         ## LIF properties
-        self.Vm      = 0    # potential (V) trace over time
+        self.Vm      = np.random.rand()    # potential (V) trace over time
         self.Rm      = 1                   # resistance (kOhm)
         self.Cm      = 10                  # capacitance (uF)
         self.tau_m   = self.Rm*self.Cm               # time constant (msec)
         self.tau_ref = 4                   # refractory period (msec)
-        self.Vth     = 1                   # spike threshold (V)
+        self.Vth     = 0.8                 # spike threshold (V)
         self.V_spike = 0.5                 # spike delta (V)
         self.act = initAct
         self.spike = False;
-        ## Stimulus
+        ## Stimulus 
         self.I       = 1.5                 # input current (A)
 
 
@@ -50,10 +52,14 @@ class neuron(object):
         if self.time > self.t_rest:
             self.Vm= self.Vm + (-self.Vm + I*self.Rm) / self.tau_m *self.dt
         if self.Vm >= self.Vth:
-            self.spike = True;
+            self.spike = True
             self.t_rest = self.time + self.tau_ref
             self.Vm = 0
         self.time += self.dt
+        if self.spike:
+            return 1.0
+        else:
+            return 0.0
         
 class layer(object):
     
@@ -62,8 +68,14 @@ class layer(object):
         for l in xrange(N):
             self.neurons.append(neuron(0,0))
             self.cnxns = connection(N,weightscale)
+    def update(self,I_init):
         
-
-
-        
+        for nn,ne in enumerate(self.neurons):
+            for nn2 in xrange(len(self.neurons)):
+                ic = ne.spike*self.cnxns.weights[nn2,nn]
+            print ne.Vm,ic
+            self.neurons[nn].update(ic + I_init)
             
+        
+        
+        
