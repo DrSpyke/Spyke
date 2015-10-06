2bd4c1e6daaac62919c248dadd959e39c976752b
DrSpyke <corey.hart.neuro@gmail.com>
2015-10-05 23:18:57 -0400

    Update SpykeDemo.py

--- SpykeDemo.py
+++ SpykeDemo.py
@@ -7,17 +7,23 @@ Created on Sun Oct 04 23:26:52 2015
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
+any later version.
 @author: Corey
 """
 from Spyke import *
 
 L = layer(100,0.1) # 100 neurons with a maximum connection weight of 0.1
-record = {}
-for l in xrange(10):
+recordIdx = {}
+recordTimes = {}
+recordNeurons = {}
+for l in xrange(len(L.neurons)):
+    recordNeurons[l] = list()
+for l in xrange(50):
     L.update(10.0)  # global drive  = 10.0
     fired = []
     for num_n,n in enumerate(L.neurons):
         if n.spike == True:
             fired.append(num_n)
-    record[l] = fired # dictionary of neurons that have fired, indexed by time index.
+            recordNeurons[num_n].append(n.time)
+    recordIdx[l] = fired # dictionary of neurons that have fired, indexed by time index.
+    recordTimes[n.time] = fired
