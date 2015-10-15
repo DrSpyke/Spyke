26f1ec1609f5de780944139b0aae3d85a61f14cb
DrSpyke <corey.hart.neuro@gmail.com>
2015-10-15 18:25:46 -0400

    Update SpykeUtils.py

--- SpykeUtils.py
+++ SpykeUtils.py
@@ -7,8 +7,19 @@ the Free Software Foundation; either version 3 of the License, or
 any later version.
 @author: Corey
 """
+import itertools
+import numpy as np
 
 class utils:
     gKeyStore = []
+   
+    def featurizer(self,record):
+        R = len(record)
+        merged = list(itertools.chain.from_iterable(record.values()))
+        f = np.zeros((len(np.unique(merged)),R))
+        for r in xrange(R):
+            print record[r]
+            f[np.array(record[r],int).reshape((-1,1)),r] = 1
+        return f
     
     
