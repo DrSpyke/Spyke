4188b2ff21629e400f17bc636808ac4a20553a33
DrSpyke <corey.hart.neuro@gmail.com>
2015-10-18 09:51:52 -0400

    Update SpykeUtils.py

--- SpykeUtils.py
+++ SpykeUtils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Sat Oct 10 17:31:32 2015
 This program is free software; you can redistribute it and/or modify
@@ -9,6 +10,7 @@ any later version.
 """
 import itertools
 import numpy as np
+import xlrd
 
 class utils:
     gKeyStore = []
@@ -22,4 +24,28 @@ class utils:
             f[np.array(record[r],int).reshape((-1,1)),r] = 1
         return f
     
+class inputs:
+    
+    inweights = np.array
+    activation = np.array
+    def __init__(self,data,numFeatures,sc):
+        self.inweights = sc*np.ones(numFeatures)
+        self.activation = np.zeros(numfeatures)
+    
+    
+def open_excel(path,lencols):
+    """
+    Open and read an Excel file
+    """
+    book = xlrd.open_workbook(path)
+  
+    first_sheet = book.sheet_by_index(0)
+    out = []
+    # read a row slice
+    for rowx in xrange(first_sheet.nrows):
+        out.append([first_sheet.row_slice(rowx,
+                                col,
+                                col+1)[0].value for col in xrange(lencols)])
+    out = np.array(out)
+    return out
     
